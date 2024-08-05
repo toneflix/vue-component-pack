80530edf8b14e7bb13ad25409106098f1bc6e8ed
3m1n3nc3 <mygames.ng@gmail.com>
2024-08-05 01:18:04 +0100

    deploy otp-input docs

--- deploy.sh
+++ deploy.sh
@@ -60,6 +60,6 @@ rm -rf "$SCRIPT_DIR/docs/.git"
 # cd -
 
 cd "$SCRIPT_DIR"
-git add -A
+git add .
 git commit -m "deploy $package docs"
 git push -f git@github.com:toneflix/toneflix.github.io.git main:toneflix-vue-docs
