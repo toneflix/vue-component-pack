726cb2e952eb004640efcfd2b78fa16d73ce37b5
3m1n3nc3 <mygames.ng@gmail.com>
2024-10-26 22:13:57 +0100

    Chore: Rebuild package

--- packages/vue-auth/dist/es/src/composables/useInlineAuth.mjs
+++ packages/vue-auth/dist/es/src/composables/useInlineAuth.mjs
@@ -1,122 +1,142 @@
-import { ref as n } from "vue";
+import { ref as r } from "vue";
 import { getAuthConfig as k, createCountdown as F } from "../config.mjs";
 import { createAuthStore as U } from "../stores/auth.mjs";
-import { storeToRefs as d } from "pinia";
-const j = () => {
-  const i = U()(), v = (m) => {
-    const l = n(), s = n();
+import { storeToRefs as x } from "pinia";
+const q = () => {
+  const m = U()(), f = (g) => {
+    const l = r(), n = r();
     return {
       send: async () => {
-        const t = await m();
-        return t.error && l.value ? l.value(t.error) : !t.error && s.value && s.value(t), t;
+        const s = await g();
+        return s.error && l.value ? l.value(s.error) : !s.error && n.value && n.value(s), s;
       },
-      onError: (t) => {
-        l.value = t;
+      onError: (s) => {
+        l.value = s;
       },
-      onSuccess: (t) => {
-        s.value = t;
+      onSuccess: (s) => {
+        n.value = s;
       }
     };
-  }, f = (m, l = k()) => {
-    const s = n(), c = n(), u = n(), a = n(), t = async () => {
-      const e = await i.login(m, l);
-      s.value = e.user, c.value = e.error, u.value = e.token, a.value = e.message;
-      const S = {
+  }, S = (g, l = k()) => {
+    const n = r(), t = r(), u = r(), c = r(!1), s = r(), i = async () => {
+      c.value = !0;
+      const e = await m.login(g, l);
+      n.value = e.user, t.value = e.error, u.value = e.token, s.value = e.message, c.value = !1;
+      const d = {
         user: e.user,
         message: e.message,
-        token: e.token
+        token: e.token,
+        loading: !1
       };
-      return e.error && (S.error = e.error), S;
-    }, { send: g, onError: o, onSuccess: r } = v(t);
+      return e.error && (d.error = e.error), d;
+    }, { send: v, onError: a, onSuccess: o } = f(i);
     return {
-      send: g,
-      user: s,
+      send: v,
+      user: n,
       token: u,
-      error: c,
-      message: a,
-      onError: o,
-      onSuccess: r
+      error: t,
+      loading: c,
+      message: s,
+      onError: a,
+      onSuccess: o
     };
-  }, w = (m, l = k()) => {
-    const s = n(), c = n(), u = n(), a = n(), t = async () => {
-      const e = await i.register(m, l);
-      return s.value = e.user, c.value = e.error, u.value = e.token, a.value = e.message, { user: e.user, error: e.error, token: e.token, message: e.message };
-    }, { send: g, onError: o, onSuccess: r } = v(t);
+  }, w = (g, l = k()) => {
+    const n = r(), t = r(), u = r(), c = r(!1), s = r(), i = async () => {
+      c.value = !0;
+      const e = await m.register(g, l);
+      return n.value = e.user, t.value = e.error, u.value = e.token, s.value = e.message, c.value = !1, {
+        user: e.user,
+        error: e.error,
+        token: e.token,
+        message: e.message,
+        loading: c.value
+      };
+    }, { send: v, onError: a, onSuccess: o } = f(i);
     return {
-      send: g,
-      user: s,
+      send: v,
+      user: n,
       token: u,
-      error: c,
-      message: a,
-      onError: o,
-      onSuccess: r
+      error: t,
+      loading: c,
+      message: s,
+      onError: a,
+      onSuccess: o
     };
-  }, E = (m = k(), l) => {
-    const s = n(), c = n(), u = async () => {
-      const o = await i.logout(m, l);
-      return s.value = o == null ? void 0 : o.error, c.value = o == null ? void 0 : o.message, { error: o == null ? void 0 : o.error, message: o == null ? void 0 : o.message };
-    }, { send: a, onError: t, onSuccess: g } = v(u);
+  }, E = (g = k(), l) => {
+    const n = r(), t = r(!1), u = r(), c = async () => {
+      t.value = !0;
+      const a = await m.logout(g, l);
+      return n.value = a == null ? void 0 : a.error, u.value = a == null ? void 0 : a.message, t.value = !1, { error: a == null ? void 0 : a.error, message: a == null ? void 0 : a.message, loading: t.value };
+    }, { send: s, onError: i, onSuccess: v } = f(c);
     return {
-      send: a,
-      error: s,
-      message: c,
-      onError: t,
-      onSuccess: g
+      send: s,
+      error: n,
+      loading: t,
+      message: u,
+      onError: i,
+      onSuccess: v
     };
-  }, h = (m, l = k()) => {
-    const s = n(), c = n(), u = n(), a = n(0), t = async () => {
-      const e = await i.forgot(m, l);
-      return s.value = e == null ? void 0 : e.error, c.value = e == null ? void 0 : e.message, u.value = e.timeout, F(u, (S) => {
-        a.value = S;
+  }, h = (g, l = k()) => {
+    const n = r(), t = r(!1), u = r(), c = r(), s = r(0), i = async () => {
+      t.value = !0;
+      const e = await m.forgot(g, l);
+      return n.value = e == null ? void 0 : e.error, u.value = e == null ? void 0 : e.message, c.value = e.timeout, t.value = !1, F(c, (d) => {
+        s.value = d;
       }), {
         error: e.error,
+        loading: t.value,
         message: e.message,
         timeout: e.timeout,
-        countdown: a.value
+        countdown: s.value
       };
-    }, { send: g, onError: o, onSuccess: r } = v(t);
+    }, { send: v, onError: a, onSuccess: o } = f(i);
     return {
-      send: g,
-      error: s,
+      send: v,
+      error: n,
+      loading: t,
+      message: u,
+      timeout: c,
+      onError: a,
+      onSuccess: o,
+      countdown: s
+    };
+  }, y = (g, l = k()) => {
+    const n = r(), t = r(), u = r(!1), c = r(), s = async () => {
+      u.value = !0;
+      const o = await m.reset(g, l);
+      return n.value = o.user, t.value = o == null ? void 0 : o.error, c.value = o == null ? void 0 : o.message, u.value = !1, { error: o.error, message: o.message, user: n.value, loading: u.value };
+    }, { send: i, onError: v, onSuccess: a } = f(s);
+    return {
+      send: i,
+      user: n,
+      error: t,
+      loading: u,
       message: c,
-      timeout: u,
-      onError: o,
-      onSuccess: r,
-      countdown: a
+      onError: v,
+      onSuccess: a
     };
-  }, y = (m, l = k()) => {
-    const s = n(), c = n(), u = n(), a = async () => {
-      const r = await i.reset(m, l);
-      return s.value = r.user, c.value = r == null ? void 0 : r.error, u.value = r == null ? void 0 : r.message, { error: r.error, message: r.message, user: s.value };
-    }, { send: t, onError: g, onSuccess: o } = v(a);
+  }, A = (g = k(), l) => {
+    const n = r(), t = r(), u = r(!1), c = r(), s = async () => {
+      u.value = !0;
+      const o = await m.loadUserFromStorage(g, l);
+      return n.value = o.user, t.value = o == null ? void 0 : o.error, c.value = o == null ? void 0 : o.message, u.value = !1, { error: o.error, message: o.message, user: n.value, loading: u.value };
+    }, { send: i, onError: v, onSuccess: a } = f(s);
     return {
-      send: t,
-      user: s,
-      error: c,
-      message: u,
-      onError: g,
-      onSuccess: o
+      send: i,
+      user: n,
+      error: t,
+      loading: u,
+      message: c,
+      onError: v,
+      onSuccess: a
     };
-  }, A = (m = k(), l) => {
-    const s = n(), c = n(), u = n(), a = async () => {
-      const r = await i.loadUserFromStorage(m, l);
-      return s.value = r.user, c.value = r == null ? void 0 : r.error, u.value = r == null ? void 0 : r.message, { error: r.error, message: r.message, user: s.value };
-    }, { send: t, onError: g, onSuccess: o } = v(a);
-    return {
-      send: t,
-      user: s,
-      error: c,
-      message: u,
-      onError: g,
-      onSuccess: o
-    };
-  }, { user: p, token: C, isAuthenticated: b } = d(i);
+  }, { user: p, token: C, isAuthenticated: b } = x(m);
   return {
     user: p,
     token: C,
     isAuthenticated: b,
     reset: y,
-    login: f,
+    login: S,
     logout: E,
     forgot: h,
     register: w,
@@ -124,5 +144,5 @@ const j = () => {
   };
 };
 export {
-  j as useInlineAuth
+  q as useInlineAuth
 };
--- packages/vue-auth/dist/es/src/stores/auth.mjs
+++ packages/vue-auth/dist/es/src/stores/auth.mjs
@@ -1,106 +1,112 @@
-import { ref as h, toValue as g } from "vue";
-import { url as m, getAuthConfig as d, createCountdown as y } from "../config.mjs";
-import u from "axios";
-import { defineStore as v } from "pinia";
-u.defaults.headers.common["Content-Type"] = "application/json; charset=utf-8";
-u.defaults.headers.common.Accept = "application/json";
-u.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+import { ref as h, toValue as d } from "vue";
+import { url as l, getAuthConfig as f, createCountdown as v } from "../config.mjs";
+import c from "axios";
+import { defineStore as y } from "pinia";
+c.defaults.headers.common["Content-Type"] = "application/json; charset=utf-8";
+c.defaults.headers.common.Accept = "application/json";
+c.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 function w() {
-  return v("vue-auth", () => {
-    const l = h({}), f = h(), i = h(!1);
+  return y("vue-auth", () => {
+    const g = h({}), m = h(), i = h(!1);
     return {
-      user: l,
-      token: f,
+      user: g,
+      token: m,
       isAuthenticated: i,
-      login: async (s, a = d()) => {
-        var o;
-        const n = m("login");
+      login: async (s, r = f()) => {
+        var n;
+        const u = l("login");
         try {
-          const { data: e } = await u.post(
-            n,
-            g(s),
-            a.axiosConfig
+          const { data: t } = await c.post(
+            u,
+            d(s),
+            r.axiosConfig
           ), {
-            user: t,
-            token: r,
-            message: c
-          } = a.transformResponse ? a.transformResponse(e) : { user: e.user, token: e.token, message: e.message };
-          return l.value = t, f.value = r, i.value = !0, localStorage.setItem(a.storageKey || "auth_token", e.token), { user: t, token: r, message: c };
-        } catch ({ response: e }) {
-          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (o = e == null ? void 0 : e.data) == null ? void 0 : o.message };
+            user: e,
+            token: a,
+            message: o
+          } = r.transformResponse ? r.transformResponse(t) : { user: t.user, token: t.token, message: t.message };
+          return g.value = e, m.value = a, i.value = !0, localStorage.setItem(r.storageKey || "auth_token", t.token), { user: e, token: a, message: o };
+        } catch (t) {
+          const { response: e } = t;
+          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (n = e == null ? void 0 : e.data) == null ? void 0 : n.message };
         }
       },
-      reset: async (s, a = d()) => {
-        var o;
-        const n = m("reset");
+      reset: async (s, r = f()) => {
+        var n;
+        const u = l("reset");
         try {
-          const { data: e } = await u.post(
-            n,
-            g(s),
-            a.axiosConfig
-          ), { user: t, message: r } = a.transformResponse ? a.transformResponse(e) : { user: e.user, message: e.message };
-          return { user: t, message: r };
-        } catch ({ response: e }) {
-          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (o = e == null ? void 0 : e.data) == null ? void 0 : o.message };
+          const { data: t } = await c.post(
+            u,
+            d(s),
+            r.axiosConfig
+          ), { user: e, message: a } = r.transformResponse ? r.transformResponse(t) : { user: t.user, message: t.message };
+          return { user: e, message: a };
+        } catch (t) {
+          const { response: e } = t;
+          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (n = e == null ? void 0 : e.data) == null ? void 0 : n.message };
         }
       },
-      logout: async (s = d(), a) => {
-        var e;
-        const n = s.getAuthHeaders ? await s.getAuthHeaders() : {}, o = m("logout");
+      logout: async (s = f(), r) => {
+        var t;
+        const u = s.getAuthHeaders ? await s.getAuthHeaders({ user: g.value, token: m.value }) : {}, n = l("logout");
         try {
-          await u.post(o, g(a), {
-            headers: { ...n },
+          await c.post(n, d(r), {
+            headers: { ...u },
             ...s.axiosConfig
-          }), l.value = {}, f.value = void 0, i.value = !1, localStorage.removeItem(s.storageKey || "auth_token");
-        } catch ({ response: t }) {
-          return { error: (t == null ? void 0 : t.data) || {}, message: (e = t == null ? void 0 : t.data) == null ? void 0 : e.message };
+          }), g.value = {}, m.value = void 0, i.value = !1, localStorage.removeItem(s.storageKey || "auth_token");
+        } catch (e) {
+          const { response: a } = e;
+          return { error: (a == null ? void 0 : a.data) || {}, message: (t = a == null ? void 0 : a.data) == null ? void 0 : t.message };
         }
       },
-      forgot: async (s, a = d()) => {
-        var e;
-        const n = a.getAuthHeaders ? await a.getAuthHeaders() : {}, o = m("forgot");
+      forgot: async (s, r = f()) => {
+        var t;
+        const u = r.getAuthHeaders ? await r.getAuthHeaders({ user: g.value, token: m.value }) : {}, n = l("forgot");
         try {
-          const { data: t } = await u.post(o, g(s), {
-            headers: { ...n },
-            ...a.axiosConfig
-          }), { timeout: r, message: c } = a.transformResponse ? a.transformResponse(t) : { timeout: t.timeout, message: t.message }, k = y(r);
-          return { timeout: r, countdown: k, message: c };
-        } catch ({ response: t }) {
-          return { error: (t == null ? void 0 : t.data) || {}, countdown: h(0), message: (e = t == null ? void 0 : t.data) == null ? void 0 : e.message };
+          const { data: e } = await c.post(n, d(s), {
+            headers: { ...u },
+            ...r.axiosConfig
+          }), { timeout: a, message: o } = r.transformResponse ? r.transformResponse(e) : { timeout: e.timeout, message: e.message }, k = v(a);
+          return { timeout: a, countdown: k, message: o };
+        } catch (e) {
+          const { response: a } = e;
+          return { error: (a == null ? void 0 : a.data) || {}, countdown: h(0), message: (t = a == null ? void 0 : a.data) == null ? void 0 : t.message };
         }
       },
-      register: async (s, a = d()) => {
-        var o;
-        const n = m("register");
+      register: async (s, r = f()) => {
+        var n;
+        const u = l("register");
         try {
-          const { data: e } = await u.post(
-            n,
-            g(s),
-            a.axiosConfig
+          const { data: t } = await c.post(
+            u,
+            d(s),
+            r.axiosConfig
           ), {
-            user: t,
-            token: r,
-            message: c
-          } = a.transformResponse ? a.transformResponse(e) : { user: e.user, token: e.token, message: e.message };
-          return l.value = t, f.value = r, i.value = !0, localStorage.setItem(a.storageKey || "auth_token", e.token), { user: t, token: r, message: c };
-        } catch ({ response: e }) {
-          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (o = e == null ? void 0 : e.data) == null ? void 0 : o.message };
+            user: e,
+            token: a,
+            message: o
+          } = r.transformResponse ? r.transformResponse(t) : { user: t.user, token: t.token, message: t.message };
+          return g.value = e, m.value = a, i.value = !0, localStorage.setItem(r.storageKey || "auth_token", t.token), { user: e, token: a, message: o };
+        } catch (t) {
+          const { response: e } = t;
+          return { user: {}, error: (e == null ? void 0 : e.data) || {}, message: (n = e == null ? void 0 : e.data) == null ? void 0 : n.message };
         }
       },
-      loadUserFromStorage: async (s = d(), a) => {
-        var e;
-        const n = localStorage.getItem(s.storageKey || "auth_token"), o = s.getAuthHeaders ? await s.getAuthHeaders() : {};
-        if (n && (f.value = n, i.value = !0, s.endpoints.profile)) {
-          const t = m("profile");
+      loadUserFromStorage: async (s = f(), r) => {
+        var t;
+        const u = localStorage.getItem(s.storageKey || "auth_token"), n = s.getAuthHeaders ? await s.getAuthHeaders({ user: g.value, token: m.value }) : {};
+        if (u && (m.value = u, i.value = !0, s.endpoints.profile)) {
+          const e = l("profile");
           try {
-            const { data: r } = await u.get(t, {
-              headers: { ...o },
-              params: { ...g(a) },
+            const { data: a } = await c.get(e, {
+              headers: { ...n },
+              params: { ...d(r) },
               ...s.axiosConfig
-            }), { user: c, message: k } = s.transformResponse ? s.transformResponse(r) : { user: r.user, message: r.message };
-            return l.value = c, { user: c, message: k };
-          } catch ({ response: r }) {
-            return { user: {}, error: (r == null ? void 0 : r.data) || {}, message: (e = r == null ? void 0 : r.data) == null ? void 0 : e.message };
+            }), { user: o, message: k } = s.transformResponse ? s.transformResponse(a) : { user: a.user, message: a.message };
+            return g.value = o, { user: o, message: k };
+          } catch (a) {
+            const { response: o } = a;
+            return { user: {}, error: (o == null ? void 0 : o.data) || {}, message: (t = o == null ? void 0 : o.data) == null ? void 0 : t.message };
           }
         }
         return { user: {} };
