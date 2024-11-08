6c0e1a37a7446f017c46fece3aa06bac3b58dcad
3m1n3nc3 <mygames.ng@gmail.com>
2024-11-08 11:50:09 +0100

    style: Update gobal types

--- package-template/src/index.ts
+++ package-template/src/index.ts
@@ -4,8 +4,8 @@ import _DummyComp from './components/dummy-comp.vue'
 
 type SFCWithInstall<T> = T & Plugin
 
-const withInstall = <T> (comp: T) => {
-  ; (comp as SFCWithInstall<T>).install = (app: App) => {
+const withInstall = <T>(comp: T) => {
+  ;(comp as SFCWithInstall<T>).install = (app: App) => {
     /* eslint-disable @typescript-eslint/no-explicit-any */
     const name = (comp as any).name
     // register component
--- packages/vue-forms/components.d.ts
+++ packages/vue-forms/components.d.ts
@@ -2,7 +2,22 @@ import type { ComponentConstructor, VueFormProps, VueFormSlots } from './src/typ
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    VueForms: ComponentConstructor<VueFormProps, VueFormSlots>
+    VueForms: ComponentConstructor<VueFormProps &
+    {
+      /**
+       * Emitted when the component needs to change the model; Is also used by v-model
+       * @param value New model value
+       */
+      'onUpdate:modelValue'?: (value: unknown) => void;
+      /**
+       * Emitted when the cancel button is clicked
+       */
+      onCancel?: () => void
+      /**
+       * Emitted when the submit button is clicked
+       */
+      onSubmit?: () => void
+    }, VueFormSlots>
   }
 }
-export {}
+export { }
