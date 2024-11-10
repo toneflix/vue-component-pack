e6af7440c5f39155442a5255184383e2d6fa28df
3m1n3nc3 <mygames.ng@gmail.com>
2024-11-10 16:42:48 +0100

    Build vue-forms

--- packages/vue-forms/dist/es/index.mjs
+++ packages/vue-forms/dist/es/index.mjs
@@ -1,10 +1,24 @@
-import { VueForms as o } from "./src/index.mjs";
-const t = {
-  install: (e) => {
-    e.use(o);
+import { VueForms as t } from "./src/index.mjs";
+import m from "./src/components/form-actions.vue.mjs";
+import n from "./src/components/input-checkbox.vue.mjs";
+import i from "./src/components/input-field.vue.mjs";
+import r from "./src/components/input-radio.vue.mjs";
+import s from "./src/components/input-select.vue.mjs";
+import c from "./src/components/input-switch.vue.mjs";
+import f from "./src/components/input-textarea.vue.mjs";
+const d = i, x = r, F = s, h = m, S = c, b = n, k = f, w = {
+  install: (o) => {
+    o.use(t);
   }
 };
 export {
-  o as VueForms,
-  t as default
+  h as FormActions,
+  b as InputCheckbox,
+  d as InputField,
+  x as InputRadio,
+  F as InputSelect,
+  S as InputSwitch,
+  k as InputTextarea,
+  t as VueForms,
+  w as default
 };
